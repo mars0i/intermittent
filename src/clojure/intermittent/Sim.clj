08a890a2bc25ed0e6454efa0aa38279ed5e4684a
mars0i <mabrams@uab.edu>
2015-05-31 16:55:12 -0500

    added more of shell of program

--- src/clojure/intermittent/Sim.clj
+++ src/clojure/intermittent/Sim.clj
@@ -6,14 +6,43 @@
            [sim.engine Steppable Schedule]
            [ec.util MersenneTwisterFast]))
 
-(gen-class :name intermittent.Sim
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(gen-class :name intermittent.World
     :extends sim.engine.SimState  ; includes signature for the start() method
     :exposes-methods {start superStart} ; alias method start() in superclass. (Don't name it 'super-start'. Use a Java name.)
     :exposes {random {:get gitRandom}, schedule {:get gitSchedule}}
     :methods []
-    :state instanceState
-    :init init-instance-state
+    :state iState
+    :init init-istate
     :main true) 
 
+;; example
+(deftype IState [x])
+(defn -init-instance-state [seed] [[seed] (IState. (atom 0.0))])
+(defn -getX [^World this] @(:x (.IState this)))
 
+(defn -main
+  [& args]
+  (sim.engine.SimState/doLoop intermittent.World (into-array String args))
+  (System/exit 0))
 
+(defn -start
+  [^World this]
+  (.superStart this)
+  ;; do stuff here
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defprotocol IndivMeth ...)
+
+(deftype Indiv [x]
+  Steppable
+  (step [this state] 
+    ;; do stuff here
+    )
+  IndivMeths
+  ;; add stuff here
+  )
+
+(defn make-indiv [] (Indiv. (atom 0.0)))
