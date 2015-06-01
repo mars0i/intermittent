59e0b10d076639b6fd69bf7e3eda5bfc2f7e1eaf
mabrams <mabrams@uab.edu>
2015-05-31 19:02:04 -0500

    bugfixes to allow compile, revised make-community, renamed World to Population

--- src/clj/intermit/Sim.clj
+++ src/clj/intermit/Sim.clj
@@ -8,10 +8,37 @@
 ;(set! *warn-on-reflection* true)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; World: class for overall system
+;; Indiv: class for individuals who communicate with each other.
+;; These could be persons, villages, subaks, etc.
+;; Initial version implements Steppable.
+
+;; Note that naming methods get_ or set_ means that the GUI will find them and
+;; allow inspection or setting of them in the UI.
+(defprotocol IndivMeths
+  (getRelig [this])
+  (set-relig! [this newval])
+  (getSuccess [this])
+  (set-success! [this newval]))
+
+(deftype Indiv [success relig]
+  Steppable
+  (step [this sim-state] 
+    (let [population ^Population sim-state]   ; why don't I have to import Population to cast with it?
+    ;; do stuff here
+    ))
+  IndivMeths
+  (getRelig [this] @relig)
+  (set-relig! [this newval] (reset! success newval))
+  (getSuccess [this] @relig)
+  (set-success! [this newval] (reset! success newval)))
+
+(import [intermit.Sim Indiv])
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Population: class for overall system
 ;; Extends SimState.
 
-(gen-class :name intermit.World
+(gen-class :name intermit.Population
     :extends sim.engine.SimState  ; includes signature for the start() method
     :exposes-methods {start superStart} ; alias method start() in superclass. (Don't name it 'super-start'. Use a Java name.)
     :exposes {random {:get gitRandom}, schedule {:get gitSchedule}}
@@ -23,51 +50,27 @@
 ;; example
 (deftype IState [indivs])
 (defn -init-instance-state [seed] [[seed] (IState. (atom []))])
-(defn -getX [^World this] @(:x (.IState this)))
+;(defn -getX [^Population this] @(:x (.IState this)))
 
 (defn -main
   [& args]
-  (sim.engine.SimState/doLoop intermit.World (into-array String args))
+  (sim.engine.SimState/doLoop intermit.Population (into-array String args))
   (System/exit 0))
 
 (defn -start
-  [^World this]
+  [this]
   (.superStart this)
-  ;; do stuff here
+  ;; make m communities of (average) size n
   )
 
 (defn make-indiv
   [this]
   (Indiv.
-    (atom (.nextDouble (.gitRandom this)))))
+    (atom (.nextDouble (.gitRandom this)))   ; relig
+    (atom (.nextDouble (.gitRandom this))))) ; success
 
 (defn make-community
-  [this num-members]
-  (swap! (.indivs this)
-         concat
-         (repeatedly #(make-indiv this) num-members)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Indiv: class for individuals who communicate with each other.
-;; These could be persons, villages, subaks, etc.
-;; Initial version implements Steppable.
-
-;; Note that naming methods get_ or set_ means that the GUI will find them and
-;; allow inspection or setting of them in the UI.
-(defprotocol IndivMeths
-  (getRelig [this])
-  (set-relig! [this newval])
-  (getSucc [this])
-  (set-succ! [this newval]))
-
-(deftype Indiv [succ relig]
-  Steppable
-  (step [this sim-state] 
-    (let [world ^World sim-state]
-    ;; do stuff here
-    ))
-  IndivMeths
-  (getRelig [this] @relig)
-  (set-relig! [this newval] (reset! succ newval))
-  (getSucc [this] @relig)
-  (set-succ! [this newval] (reset! succ newval)))
+  [this size]
+  (let [members (repeatedly #(make-indiv this) size)]
+    ;; add members to community here
+    (swap! (.indivs this) concat members))) ; add members to entire population
