d014e32a5d66622604bdaee1b4e0581679cb3f41
mabrams <mabrams@uab.edu>
2015-06-25 11:48:11 -0500

    Experiments to deal with choosing/ordering bugs

--- notinuse/Shuffling.clj
+++ notinuse/Shuffling.clj
@@ -4,3 +4,33 @@
         (.shuffle pop-bag rng)                                         ; CONSIDER using a Clojure shuffle routine, or use Bags more of the time to avoid conversion
         ;(copy-relig! this sim (vec pop-bag))))
         ;(copy-relig! this sim (shuffle population)))) ; JUST AS slow as the Bag version. Clojure shuffle throws coll into an ArrayList and then calls java.util.Collections/shuffle on it.
+
+;; Can I avoid repeated accesses to the same field, caching them?  Does it matter?
+;; 
+;; TODO: BUG: When there are many with the max success, this is biased toward the end of the sequence.
+;; Why? This is OK when the seq is in random order, but it shouldn't happen.
+;; Ah, this is why: Because the random choice of the winner does pairwise comparisons.
+;; So it randomly chooses one from the first pair, but then the winner of that randomly competes
+;; against the next, and so on.  Therefore prob that the first indiv at max will win is 0.5^n,
+;; where n is the number of indivs with the max value.  (It's also bad that I'm running params that
+;; tend to force success values to 0.0 or 1.0.)
+;;
+;; Note that the analogous procedure in LKJPlus.nlogo, find-best, uses NetLogo's ask, which means
+;; that subaks are always compared in random order.  In effect they're shuffled before the comparison
+;; process starts.
+;;
+(defn choose-most-successful
+  "Given a collection of Indiv's, returns the one with the greatest success, or
+  nil if the collection is empty."
+  ^Indiv [^MersenneTwisterFast rng models]
+  (letfn [(compare-success 
+            ([] nil) ; what reduce does if collection is empty
+            ([^Indiv i1 ^Indiv i2]
+             (let [^double success1 (getSuccess i1)
+                   ^double success2 (getSuccess i2)]
+               (cond (== success1 success2) (if (< ^double (.nextDouble rng) 0.5) ; a rare case, but we don't want ties' results to be path dependent.
+                                              i1
+                                              i2)
+                     (> success1 success2) i1
+                     :else i2))))]
+    (reduce compare-success models)))
--- src/clj/intermit/Sim.clj
+++ src/clj/intermit/Sim.clj
@@ -261,16 +261,10 @@
   (let [add-relig (fn [^double acc ^Indiv indiv] (+ acc ^double (getRelig indiv)))]
     (reduce add-relig init-value indivs)))
 
-(defn choose-others-from-pop
-  "Randomly sample a Poisson-distributed number of indivs from population,
-  excluding me.  (The mean for the Poisson distribution is stored in the
-  poisson object.)"
-  [^MersenneTwisterFast rng ^Poisson poisson me population]
-  (let [size-wout-me (dec (count population)) ; we're sampling indivs *other* than me; there are count - 1 of them.
-        rand-num (.nextInt poisson)
-        num-to-choose (if (< rand-num size-wout-me) rand-num size-wout-me)] ; When Poisson mean is large, result may be larger than number of indivs.
-    (sample-wout-repl-or-me rng num-to-choose me population)))
-
+;; TODO BUG: Returns a set, at present.  There's no reason to think that when
+;; converted to a vector, the result is in random order,
+;; but I need for it to be random if choose-most-successful requires that.
+;;
 ;; It's much faster to remove the originating Indiv from samples here,
 ;; rather than removing it from the collection to be sampled, at least
 ;; for reasonably large populations.  Makes this function more complicated, 
@@ -281,7 +275,8 @@
   items identical? to me.  Returns a vector or a set; if you want something
   more specific, it's the caller's responsibility.  Should be fastest when
   coll has fast index access (e.g. it's a vector) and when the elements hash
-  by identity (e.g. when defined by deftype rather than defrecord)."
+  by identity (e.g. when defined by deftype rather than defrecord).  NOTE:
+  results are not in random order."
   [^MersenneTwisterFast rng ^long num-samples me coll]
   (let [size (count coll)
         size-wout-me (dec size)] ; how many elements, excluding me?
@@ -294,30 +289,110 @@
             (recur sample-set)       ; lose it
             (recur (conj sample-set sample))))))))
 
+;; By amalloy, from https://gist.github.com/amalloy/805546:
+;; See also http://stackoverflow.com/questions/3944556/what-if-anything-is-wrong-with-this-shuffling-algorithm-and-how-can-i-know
+(defn lazy-shuffle [coll]
+  ((fn shuffle [^clojure.lang.ITransientVector coll]
+     (lazy-seq
+      (let [c (count coll)]
+        (when-not (zero? c)
+          (let [n (rand-int c)]
+            (cons (get coll n)
+                  (shuffle (.pop (assoc! coll n (get coll (dec c)))))))))))
+   (transient (vec coll))))
+
+;; By amalloy, from https://gist.github.com/amalloy/805546, with small mod by Marshall:
+;; See also:
+;; https://gist.github.com/pepijndevos/805747
+;; http://stackoverflow.com/questions/3944556/what-if-anything-is-wrong-with-this-shuffling-algorithm-and-how-can-i-know
+(defn take-randnth [rng num coll]
+  (take num
+        (rest
+         (map first
+              (iterate (fn [[ret items]]
+                         (let [idx (.nextInt rng (count items))]
+                           [(items idx)
+                            (subvec (assoc items idx (items 0))
+                                    1)]))
+                       [nil
+                        (vec coll)])))))
+
+;; by Pepijn de Vos, pepijndevos from https://gist.github.com/pepijndevos/805747, with small mod by Marshall
+; reduce, reorder, subvec, O(m)
+(defn take-rand3 [rng nr coll]
+  (let [len (count coll)
+        ; why doesn't rand-int take a start?
+        rand-int (fn [lo hi] (+ lo (.nextInt rng (- hi lo))))]
+    (subvec (->> (range nr)
+                 (reduce #(conj %1 [%2 (rand-int %2 len)]) [])
+                 (reduce
+                   (fn swap [a [i b]]
+                      (assoc a b (get a i) i (get a b)))
+                   coll))
+            0 nr)))
+
+;; from https://gist.github.com/pepijndevos/805747, by amalloy (?), with small mod by Marshall
+; amalloy, O(m)
+(defn take-rand4 [rng nr coll]
+  (first
+   (nth
+    (iterate (fn [[ret candidates]]
+               (let [idx (.nextInt rng (count candidates))]
+                 [(conj ret (candidates idx))
+                  (subvec (assoc candidates idx (candidates 0))
+                          1)]))
+             [[]
+              coll])
+    nr)))
+
+;; from https://gist.github.com/pepijndevos/805747, by amalloy (?), with small mod by Marshall
+; amalloy, o(mg)
+(defn take-rand5 [rng nr coll]
+  (take nr
+        ((fn shuffle [coll]
+           (lazy-seq
+             (let [c (count coll)]
+               (when-not (zero? c)
+                 (let [n (.nextInt rng c)]
+                   (cons (get coll n)
+                         (shuffle (pop! (assoc! coll n (get coll (dec c)))))))))))
+           (transient coll))))
+
+(defn choose-others-from-pop
+  "Randomly sample a Poisson-distributed number of indivs from population,
+  excluding me.  (The mean for the Poisson distribution is stored in the
+  poisson object.)"
+  [^MersenneTwisterFast rng ^Poisson poisson me population]
+  (let [size-wout-me (dec (count population)) ; we're sampling indivs *other* than me; there are count - 1 of them.
+        rand-num (.nextInt poisson)
+        num-to-choose (if (< rand-num size-wout-me) rand-num size-wout-me)] ; When Poisson mean is large, result may be larger than number of indivs.
+    (take-rand3 rng num-to-choose population) ;; TODO BUG NOT RIGHT INCLUDES me
+    ;(sample-wout-repl-or-me rng num-to-choose me population)
+    ))
+
+
 ;; Can I avoid repeated accesses to the same field, caching them?  Does it matter?
 ;; 
-;; TODO: BUG: When there are many with the max success, this is biased toward the end of the sequence.
-;; Why? This is OK when the seq is in random order, but it shouldn't happen.
-;; Ah, this is why: Because the random choice of the winner does pairwise comparisons.
-;; So it randomly chooses one from the first pair, but then the winner of that randomly competes
-;; against the next, and so on.  Therefore prob that the first indiv at max will win is 0.5^n,
-;; where n is the number of indivs with the max value.  (It's also bad that I'm running params that
-;; tend to force success values to 0.0 or 1.0.)
+;; Note that the analogous procedure in LKJPlus.nlogo, find-best, uses NetLogo's ask, which means
+;; that subaks are always compared in random order.  In effect they're shuffled before the comparison
+;; process starts.
+;;
 (defn choose-most-successful
   "Given a collection of Indiv's, returns the one with the greatest success, or
-  nil if the collection is empty."
+  nil if the collection is empty.  NOTE if there are ties, this will always
+  use the first one found, so collection must already be in random order if you want
+  a random member of the winners."
   ^Indiv [^MersenneTwisterFast rng models]
   (letfn [(compare-success 
             ([] nil) ; what reduce does if collection is empty
             ([^Indiv i1 ^Indiv i2]
              (let [^double success1 (getSuccess i1)
                    ^double success2 (getSuccess i2)]
-               (cond (== success1 success2) (if (< ^double (.nextDouble rng) 0.5) ; a rare case, but we don't want ties' results to be path dependent.
-                                              i1
-                                              i2)
+               (cond (== success1 success2) i1 ; NOTE always returns the first winner
                      (> success1 success2) i1
                      :else i2))))]
     (reduce compare-success models)))
+    ;(reduce compare-success (take-rand3 rng (count models) models))))
 
 ;; TODO note this means that e.g. if indiv is isolated, then when it happens to get high relig, it will also have high success.  Is that realistic?
 (defn calc-success
